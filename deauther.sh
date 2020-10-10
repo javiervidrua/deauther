03dcc2cde2e34e3be599f77c38308bd1b6f37b1e
Javier Vidal Ruano <67796321+javiervidrua@users.noreply.github.com>
2020-10-10 23:12:33 +0200

    Trapped SIGINT, implemented monitorMode and managedMode

--- deauther.sh
+++ deauther.sh
@@ -40,22 +40,47 @@ function checkExitCode(){
         fi
 }
 
-function getWirelessInterface () {
-        INTERFACE=$1
-        INTERFACE_UP=0
+function checkMonitorMode(){
+        local INTERFACE=$1
+        local MODE=$(iwconfig 2>/dev/null | grep -E "${INTERFACE}" -A1 | grep "Mode" | cut -d ':' -f 2 | cut -d' ' -f1 | cut -d' ' -f1)
+        echo "[*] Interface ${INTERFACE} in ${MODE} mode"
+        if [ $MODE = 'Monitor' ]; then
+                return 0
+        else
+                return 1
+        fi
+}
+
+function checkWirelessInterface () {
+        local INTERFACE=$1
+        local INTERFACE_FOUND=0
         for INT in $(ifconfig -a | sed 's/[ \t].*//;/^$/d' | grep 'w' | tr -d ':'); do
-                if [ $INT = $INTERFACE ]; then
-                        INTERFACE_UP=1
+                if [ $INTERFACE = $INT ]; then
+                        INTERFACE_FOUND=1
                         break
                 fi
         done
-        if [ $INTERFACE_UP -eq 0 ]; then
+        if [ $INTERFACE_FOUND -eq 0 ]; then
                 echo "[-] Error: Could not detect any wireless interfaces up and running"
                 return 1
         else
-                echo "[*] Wireless interface ${INTERFACE} up and running"
+                echo "[*] Wireless interface ${INTERFACE} is up and running"
                 return 0
         fi
+}
+
+function monitorMode(){
+        trap '' 2 # SIGINT
+        INTERFACE=$1
+        echo "[*] Putting interface ${INTERFACE} in monitor mode"
+        ifconfig ${INTERFACE} down && iwconfig ${INTERFACE} mode monitor && ifconfig ${INTERFACE} up && echo "[*] Interface ${INTERFACE} is now on monitor mode" && return 0 || checkExitCode $? "monitorMode"
+}
+
+function managedMode(){
+        trap '' 2 # SIGINT
+        INTERFACE=$1
+        echo "[*] Putting interface ${INTERFACE} in managed mode"
+        ifconfig ${INTERFACE} down && iwconfig ${INTERFACE} mode managed && ifconfig ${INTERFACE} up && echo "[*] Interface ${INTERFACE} is now on managed mode" && return 0 || checkExitCode $? "managedMode"
 }
 
 function usage(){
@@ -65,10 +90,23 @@ function usage(){
 # MAIN
 checkArguments $@
 checkExitCode $? "checkArguments"
-getWirelessInterface $1
-checkExitCode $? "getWirelessInterface"
+
+checkWirelessInterface $1
+checkExitCode $? "checkWirelessInterface"
+
+INTERFACE=$1
+checkMonitorMode $INTERFACE
+if [ $? -eq 1 ]; then
+        monitorMode $INTERFACE
+        checkExitCode $? "monitorMode"
+fi
+
 # use to get the SSIDs:
-#  sudo iw dev wlan0 scan | egrep "signal:|SSID:" | sed -e "s/\tsignal: //" -e "s/\tSSID: //" | awk '{ORS = (NR % 2 == 0)? "\n" : " "; print}' | sort
+#   sudo iw dev wlan0 scan | egrep "signal:|SSID:" | sed -e "s/\tsignal: //" -e "s/\tSSID: //" | awk '{ORS = (NR % 2 == 0)? "\n" : " "; print}' | sort
 
 # use to deauth
-#  mdk3
+#   mdk3
+
+echo "[*] Done"
+managedMode $INTERFACE
+checkExitCode $? "managedMode"
